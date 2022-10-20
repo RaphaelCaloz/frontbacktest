099ebfb31f001b19034e66ec05be15ceaffeb172
calozrahael <calozraphael@gmail.com>
2022-10-20 19:52:10 -0400

    Add xkcd endpoints to backend API

--- server.js
+++ server.js
@@ -1,18 +1,35 @@
 const express = require('express')
 const path = require('path')
+const request = require('request')
 
 const app = express()
 const port = process.env.PORT || 5000
 
 app.use(express.static(path.join(__dirname, 'build')))
 
-// all your api code here
+// all backend api code here
+app.get('/api/xkcd/:comicId', (req, res) => {
+    // If comicId is valid, get that comic's data
+    request(`https://xkcd.com/${req.params.comicId}/info.0.json`, { json: true }, (err, response, body) => {
+        if (err) { return console.log(err) }
+        res.json(body)
+    })
+})
+
+app.get('/api/xkcd', (req, res) => {
+    request(`https://xkcd.com/info.0.json`, { json: true }, (err, response, body) => {
+        if (err) { return console.log(err) }
+        res.json(body)
+    })
+})
+
 app.get('/api', (req, res) => {
     res.json({
         message: 'This is the api endpoint'
     })
 })
 
+// Default routes
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
