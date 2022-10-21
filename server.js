7eaf6923350e0b7e8e1aaea7311fd291e66258d5
calozrahael <calozraphael@gmail.com>
2022-10-21 01:39:52 -0400

    Add buttons and text

--- server.js
+++ server.js
@@ -7,7 +7,16 @@ const port = process.env.PORT || 5000
 
 app.use(express.static(path.join(__dirname, 'build')))
 
-// all backend api code here
+// All backend api routes
+app.get('/api/xkcd/maxComicId', (req, res) => {
+    request(`https://xkcd.com/info.0.json`, { json: true }, (err, response, body) => {
+        if (err) { return console.log(err) }
+        res.json({
+            maxComicId: body.num
+        })
+    })
+})
+
 app.get('/api/xkcd/:comicId', (req, res) => {
     // If comicId is valid, get that comic's data
     request(`https://xkcd.com/${req.params.comicId}/info.0.json`, { json: true }, (err, response, body) => {
@@ -29,7 +38,7 @@ app.get('/api', (req, res) => {
     })
 })
 
-// Default routes
+// Default routes. Used to access the frontend.
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
