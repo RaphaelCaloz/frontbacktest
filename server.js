23ee29a5eddb68861b9ce519eaf13278ec8a729d
calozrahael <calozraphael@gmail.com>
2022-10-22 18:53:41 -0400

    Complete psql GET endpoint

--- server.js
+++ server.js
@@ -33,22 +33,41 @@ app.get('/api/xkcd', (req, res) => {
     })
 })
 
-app.get('/api/psql.increment.comic/:comicId', (req, res) => {
-    // const client = new Client({
-    //     // user: 'weqlqgagxqhbkc',
-    //     // host: 'ec2-44-210-228-110.compute-1.amazonaws.com',
-    //     // database: 'd9h93g0deaqdos',
-    //     // password: 'da49d5cd8848c80cc776e18cf267d3735fc9ddd863991a326be455ece7be326c',
-    //     // port: '5432',
-    //     connection: 
-    //     ssl: true
-    // });
+app.get('/api/psql/comicViews/:comicId', (req, res) => {
+    const client = new Client({
+        //connectionString: 'postgres://weqlqgagxqhbkc:da49d5cd8848c80cc776e18cf267d3735fc9ddd863991a326be455ece7be326c@ec2-44-210-228-110.compute-1.amazonaws.com:5432/d9h93g0deaqdos',
+        connectionString: process.env.DATABASE_URL,  // Replace by connectionString for local testing
+        ssl: { rejectUnauthorized: false }
+    })
     // console.log("#### CLIENT ###")
     // console.log(client)
-    res.json({
-        connectionString: process.env.DATABASE_URL
+
+
+    // res.json({
+    //     connectionString: process.env.DATABASE_URL
+    // })
+
+
+    client.connect()
+
+    // This query should return at most 1 entry, as "comic_num" column values are unique
+    client.query(`SELECT comic_views FROM views WHERE comic_num = ${req.params.comicId}`, (err, psqlRes) => {
+        if (err) { return console.error(err) }
+        // console.log("typeof: ", typeof psqlRes.rows)
+        // console.log("len: ", psqlRes.rowCount)
+        for (let row of psqlRes.rows) {
+            console.log(JSON.stringify(row))
+        }
+        let numViews = 0
+        if (psqlRes.rowCount > 0) {
+            numViews = psqlRes.rows[0].comic_views
+        }
+
+        res.json({
+            comicViews: numViews
+        })
     })
-    // client.connect()
+})
     // Check if the entry for comicId exists
     // const query = `SELECT comic_views FROM views WHERE comic_num = ${req.params.comicId}`
     // client.query(query, (err, sqlResponse) => {
@@ -60,7 +79,7 @@ app.get('/api/psql.increment.comic/:comicId', (req, res) => {
     //         message: "psql endpoint"
     //     })
     // })
-})
+
 
 // Default routes. Used to access the frontend.
 app.get('/*', (req, res) => {
